ba27117149ef99bd9c3e33b286854ea213de9938
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2024-09-04 18:27:19 -0500

    change: car models

--- my_first_app/models.py
+++ my_first_app/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 class Car(models.Model):
     name = models.TextField(max_length=250)
+    year = models.TextField(max_length=4, null=True)
+    color = models.TextField(max_length=50, null=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.year}"
+
