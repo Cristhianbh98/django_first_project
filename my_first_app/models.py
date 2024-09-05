63aef6f84c55ab095844a9c13e570693ee870849
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2024-09-04 20:47:31 -0500

    add: car, plublisher, author, profile, book

--- my_first_app/models.py
+++ my_first_app/models.py
@@ -9,3 +9,34 @@ class Car(models.Model):
     def __str__(self):
         return f"{self.name} - {self.year}"
 
+class Publisher(models.Model):
+    name = models.TextField(max_length=250, null=True)
+    address = models.TextField(max_length=250, null=True)
+    city = models.TextField(max_length=100, null=True)
+    state_province = models.TextField(max_length=100, null=True)
+    country = models.TextField(max_length=100)
+    website = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.TextField(max_length=250)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    author = models.OneToOneField(Author, on_delete=models.CASCADE)
+    website = models.URLField()
+    biography = models.TextField(max_length=250)
+
+class Book(models.Model):
+    title = models.TextField(max_length=250)
+    publication_date = models.DateField()
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author, related_name='authors')
+
+    def __str__(self):
+        return self.title
